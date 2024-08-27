caaf0e1f7fc8279d97f47f2567c2c34c3b5b8e0a
BTurner08 <mrbturner2013@yahoo.com>
2024-08-26 20:39:57 -0400

    class 5 updated

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .forms import ContactForm
+from django.core.mail import send_mail
 
 def about_view(request):
     return render(request, 'pages/about.html')
@@ -8,7 +9,20 @@ def contact_views(request):
     if request.method == "POST":
         form = ContactForm(request.POST)
         print("POST...")
+        
+        if form.is_valid():
+            name = form. cleaned_data["name"]
+            email_from = form. cleaned_data["email"]
+            message = form.cleaned_data["message"]
+
+            send_mail(
+                "Email from " + name, message, email_from, ["airblake2016@gmail.com"]
+            )
+        else:
+            print("Invalid Form")
+
+        print("POST ... ")
     else:
         print("GET...")
-    form = ContactForm()
+        form = ContactForm()
     return render(request, "pages/contact.html", {"form": form})
